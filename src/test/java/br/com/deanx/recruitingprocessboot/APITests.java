c7a48cedc36f65837d6519b3b723105bec70361f
Alex Costa <alex@deanx.com.br>
2018-01-06 18:58:19 +0100

    Fix content-type and test URL

--- src/test/java/br/com/deanx/recruitingprocessboot/APITests.java
+++ src/test/java/br/com/deanx/recruitingprocessboot/APITests.java
@@ -5,9 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.http.MediaType.parseMediaType;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,8 +24,12 @@ public class APITests {
 
     @Test
     public void givenOffers_whenGetOffers_theStatus200() throws Exception {
-        this.mockMvc.perform(get("/api/offers")).andExpect(status().isOk())
-                .andExpect(content().contentType("application/json")
+        this.mockMvc.perform(get("/offers")
+                .accept(parseMediaType("application/json"))
+        ).andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)
         );
     }
+
+
 }
