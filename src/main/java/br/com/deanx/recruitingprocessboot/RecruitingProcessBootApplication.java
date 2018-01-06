d5601985050fee4100dd9580d0dd9cfe203666e9
Alex Costa <alex@deanx.com.br>
2018-01-06 17:57:36 +0100

    Initial commit

---
+++ src/main/java/br/com/deanx/recruitingprocessboot/RecruitingProcessBootApplication.java
@@ -0,0 +1,12 @@
+package br.com.deanx.recruitingprocessboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitingProcessBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitingProcessBootApplication.class, args);
+	}
+}
