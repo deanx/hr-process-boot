4ef9c4eeb1bfac33168233a1ba91de5672317922
Alex Costa <alex@deanx.com.br>
2018-01-06 18:31:31 +0100

    Offers API status and content-type test

---
+++ src/test/java/br/com/deanx/recruitingprocessboot/APITests.java
@@ -0,0 +1,29 @@
+package br.com.deanx.recruitingprocessboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = RecruitingProcessBootApplication.class)
+@AutoConfigureMockMvc
+public class APITests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void givenOffers_whenGetOffers_theStatus200() throws Exception {
+        this.mockMvc.perform(get("/")).andExpect(status().isOk())
+                .andExpect(content().contentType("application/json")
+        );
+    }
+}
