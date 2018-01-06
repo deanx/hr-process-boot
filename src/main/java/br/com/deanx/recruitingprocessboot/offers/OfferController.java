cea98d2e423f28188ab3a573d59325828ce9de13
Alex Costa <alex@deanx.com.br>
2018-01-06 21:22:30 +0100

    Attend to givenOffers_whenGetOfferById_thenStatus200 test

--- src/main/java/br/com/deanx/recruitingprocessboot/offers/OfferController.java
+++ src/main/java/br/com/deanx/recruitingprocessboot/offers/OfferController.java
@@ -4,10 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @EnableAutoConfiguration
@@ -16,12 +15,21 @@ public class OfferController {
     private OfferRepository repository;
 
     @RequestMapping(method = RequestMethod.GET, value = "/offers")
-    String listOffers() {
-        return "";
+    ResponseEntity<List<Offer>> listAll(){
+        return new ResponseEntity<>((List<Offer>)repository.findAll(), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value="/offers/{offerId}")
+    public ResponseEntity findOffer(@PathVariable Long offerId) {
+        List<Offer> offers = repository.findById(offerId);
+        return 1 == offers.size() ? new ResponseEntity<>(offers.get(0), HttpStatus.OK)
+                : new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 
     @RequestMapping(method = RequestMethod.POST, value="/offers")
     ResponseEntity<Offer> saveOffer(@RequestBody Offer offer) {
         return new ResponseEntity<>(repository.save(offer), HttpStatus.CREATED);
     }
+
+
 }
--- src/test/java/br/com/deanx/recruitingprocessboot/APITests.java
+++ src/test/java/br/com/deanx/recruitingprocessboot/APITests.java
@@ -1,5 +1,8 @@
 package br.com.deanx.recruitingprocessboot;
 
+import br.com.deanx.recruitingprocessboot.offers.Offer;
+import br.com.deanx.recruitingprocessboot.offers.OfferRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +12,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.http.MediaType.parseMediaType;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import java.time.LocalDate;
 
+import static org.hamcrest.Matchers.is;
+import static org.springframework.http.MediaType.parseMediaType;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = RecruitingProcessBootApplication.class)
@@ -20,6 +26,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class APITests {
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private OfferRepository offerRepository;
 
     @Test
     public void givenOffers_whenGetOffers_theStatus200() throws Exception {
@@ -30,6 +39,10 @@ public class APITests {
         );
     }
 
+    @Before
+    public void setUp() {
+        offerRepository.deleteAll();
+    }
 
     @Test
     public void givenApplications_whenGetApplications_theStatus200() throws Exception {
@@ -50,5 +63,22 @@ public class APITests {
                 "}").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    public void givenOffers_whenGetOfferById_thenStatus200() throws Exception {
+        Offer offer = new Offer();
+        offer.setJobTitle("Senior developer");
+        offer.setStartDate(LocalDate.now());
+        offer.setNumberOfApplications(1);
+
+        Offer newOffer = offerRepository.save(offer);
+        Long newOfferId = newOffer.getId();
+        mockMvc.perform(get("/offers/" + newOfferId)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.jobTitle",is("Senior developer")));
+
     }
 }
