a69d60809023aaebdb77aecb7860b189beb44d1e
Alex Costa <alex@deanx.com.br>
2018-01-07 23:59:15 +0100

    Enhance applyToOffer logic

--- src/main/java/br/com/deanx/recruitingprocessboot/offer/OfferService.java
+++ src/main/java/br/com/deanx/recruitingprocessboot/offer/OfferService.java
@@ -4,17 +4,13 @@ import br.com.deanx.recruitingprocessboot.application.Application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class OfferService {
     @Autowired
     private OfferRepository repository;
 
     public void applyToOffer(Offer offer, Application application) {
-        List<Application> applicationList = offer.getApplicationList();
-        applicationList.add(application);
-        offer.setApplicationList(applicationList);
+        offer.getApplicationList().add(application);
         repository.save(offer);
     }
 }
